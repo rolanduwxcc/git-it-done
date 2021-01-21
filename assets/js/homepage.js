24ed08a9d0f2a0d51740cfdd669d0c094bf2cd43
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-21 00:19:02 -0600

    display user lookup form and repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,64 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+//--------------------------------VARIABLES
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos"
+
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            displayRepos(data, user);
+        });
+    });
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    //clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    //loop over repos
+    for (let i = 0; i < repos.length; i++) {
+        const element = repos[i];
+        
+        //format repo name
+        var repoName = element.owner.login + "/" + element.name;
+
+        //create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        //create a span element to hold repositor name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //append to container
+        repoEl.appendChild(titleEl);
+
+        //append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
