663e3d1c1cb779dfb43a6e19d593eb3e846d2d5c
rolanduwxcc <rolanduwxcc@gmail.com>
2021-01-21 18:56:13 -0600

    error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,9 +10,18 @@ var getUserRepos = function(user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+        //request was successful
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error) {
+        //Notice this '.catch()' getting chained on the end of '.then()' method
+        alert("Unable to connect to GitHub-wlr");
     });
 };
 
@@ -34,6 +43,11 @@ var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
 
+    //check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositoreis found.";
+        return;
+    }
     //clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
